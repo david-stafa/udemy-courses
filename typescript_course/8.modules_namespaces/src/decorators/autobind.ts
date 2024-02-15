67b4cd078c229c2f4d040989d90775d7baebc065
David Štafa <david.stafa@gmail.com>
2024-02-15 15:00:06 +0100

    Namespaces and ES modules finished

---
+++ typescript_course/8.modules_namespaces/src/decorators/autobind.ts
@@ -0,0 +1,18 @@
+// Autobind decorator
+export function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalValue = descriptor.value;
+  //   console.log(_)
+  //   console.log(_2)
+  //   console.log(descriptor)
+  //   console.log(originalValue)
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalValue.bind(this);
+      console.log(boundFn);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
